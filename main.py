c693b26944fc42cf51ea2222fe0410a1d23f7d02
ileana <ileana@ileanas-Air.fios-router.home>
2021-01-05 18:01:12 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Author: Ileana Hernandez
+# Date: 01/05/2021
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = "horse"
+print("Your favorite animal is the " + fave_animal + ".")
